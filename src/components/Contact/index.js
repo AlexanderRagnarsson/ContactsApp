ffb3b9dd01f86acc85b71c9feb39dddf4bd8abaf
BentGunnarsson <bent19@ru.is>
2021-11-29 19:07:20 +0000

    Contact screen with touchable phone number

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import { Text, View, Animated } from 'react-native';
+import {
+  Text, View, Animated, TouchableHighlight,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 import styles from './styles';
+import callNumber from '../../services/phonecalls';
 
 const Contact = ({
   id, name, phoneNumber, photo,
 }) => (
-  <View style={styles.View}>
-    <Text style={styles.Text}>{id}</Text>
+  <View style={styles.view}>
+    {/* <View>
+      <Text style={styles.Text}>{id}</Text>
+    </View> */}
     <Animated.Image
       style={styles.photo}
       source={{ uri: photo }}
     />
-    <View style={styles.NameView}>
+    <View style={styles.nameView}>
       <Text style={styles.nameText}>{name}</Text>
     </View>
-    <View style={styles.phoneNumberView}>
-      <Text style={styles.phoneNumberText}>{phoneNumber}</Text>
-    </View>
+    <TouchableHighlight
+      style={styles.phoneNumberView}
+      onPress={() => callNumber(`${phoneNumber}`)}
+    >
+      <View style={styles.iconNumber}>
+        <AntDesign name="phone" size={60} color="black" />
+        <Text style={styles.phoneNumberText}>{phoneNumber}</Text>
+      </View>
+    </TouchableHighlight>
+    <TouchableHighlight
+      style={styles.phoneNumberView}
+    >
+      <View style={styles.iconNumber}>
+        <AntDesign name="edit" size={60} color="black" />
+        <Text style={styles.phoneNumberText}>Edit contact</Text>
+      </View>
+    </TouchableHighlight>
   </View>
 );
 
--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -1,8 +1,42 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  View: {
+    flex: 1,
+  },
+  view: {
+    // flex: 3,
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
   photo: {
-    width: 200,
-    height: 200,
+    width: 300,
+    height: 300,
+    borderRadius: 300 / 2,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  nameView: {
+    paddingVertical: 15,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  nameText: {
+    fontSize: 48,
+    fontWeight: 'bold',
+  },
+  phoneNumberView: {
+    paddingVertical: 15,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  phoneNumberText: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    paddingHorizontal: 15,
+  },
+  iconNumber: {
+    flexDirection: 'row',
   },
 });
