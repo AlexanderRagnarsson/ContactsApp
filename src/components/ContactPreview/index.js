024588984b2f32584c3b4ea2bcc6d98824285bbe
AlexanderRagnarsson <alexanderrag19@ru.is>
2021-11-29 12:10:23 +0000

    Adding views

--- src/components/ContactPreview/index.js
+++ src/components/ContactPreview/index.js
@@ -1,23 +1,33 @@
 import React from 'react';
 // import { useSelector } from 'react-redux';
-import { View, Text, Animated, TouchableHighlight } from 'react-native';
+import {
+  View, Text, Animated, TouchableHighlight,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
-const ContactPreview = ({ name, photo }) => (
+const ContactPreview = ({
+  id, name, photo, navigate,
+}) => (
   <View style={styles.PreviewView}>
-    {/* <TouchableHighlight style={styles.touchable}> */}
-    <Animated.Image
-      style={styles.photo}
-      source={photo}
-    />
-    <Text style={styles.name}>{name}</Text>
+    <TouchableHighlight
+      style={styles.touchable}
+      onPress={() => navigate('ContactDetail', { id })}
+    >
+      <Animated.Image
+        style={styles.photo}
+        source={photo}
+      />
+      <Text style={styles.name}>{name}</Text>
+    </TouchableHighlight>
   </View>
 );
 
 ContactPreview.propTypes = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   photo: PropTypes.string.isRequired,
+  navigate: PropTypes.func.isRequired,
 };
 
 export default ContactPreview;
